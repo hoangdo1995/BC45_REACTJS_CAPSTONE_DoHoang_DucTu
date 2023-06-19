8baa5d882714a861739a780c01b81dd252f308ce
dohoang790@gmail.com <dohoang790@gmail.com>
2023-06-19 20:19:11 +0700

    config file

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import {history} from '../../index.js'
+import { USER_LOGIN, getStorageJson,http,setStorageJson } from '../../templates/config.js'
+import axios from 'axios'
 
 const Login = () => {
+  http.post('/api/Users/signin',{
+    "email": "đă@gmail.com",
+    "password": "123"
+  });
+  
+  
   return (
     <div>
-      Login
+        login
     </div>
   )
 }
--- src/templates/config.js
+++ src/templates/config.js
@@ -1,18 +1,59 @@
+import axios from "axios";
 import { json } from "react-router-dom";
+import { history } from "..";
 
 const USER_LOGIN = 'user_login';
+const TOKEN = 'accessToken';
+const DOMAIN = 'https://shop.cyberlearn.vn';
 
-const getStorageJson = (item)=>{
-    if(localStorage.getItem(item)){
-        const itemJson = localStorage.getItem(item);
-        const itemObject = JSON.parse(itemJson);
-        return itemObject;
+const config = {
+    getStorageJson:(name)=>{
+        if(localStorage.getItem(name)){
+            const itemJson = localStorage.getItem(name);
+            const itemObject = JSON.parse(itemJson);
+            return itemObject;
+        }
+        return null;
+    },
+    setStorageJson:(name,data)=>{
+        const itemJson = JSON.stringify(data);
+        localStorage.setItem(name,itemJson);
     }
-    return null;
-}
-const setStorageJson = (item,data)=>{
-    const itemJson = JSON.stringify(item);
-    localStorage.setItem(itemJson,data);
 }
 
-export {getStorageJson,USER_LOGIN};
+export const {getStorageJson,setStorageJson} = config;
+export {USER_LOGIN,TOKEN,DOMAIN};
+
+export const http = axios.create({
+    baseURL:'https://shop.cyberlearn.vn',
+    timeout:3000
+});
+
+// cấu hình interceptor cấu hình cho request (api gửi đi)
+http.interceptors.request.use((req)=>{
+    req.headers = {...req.headers,
+        TokenCyberSoft:
+        `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA0NSIsIkhldEhhblN0cmluZyI6IjIwLzExLzIwMjMiLCJIZXRIYW5UaW1lIjoiMTcwMDQzODQwMDAwMCIsIm5iZiI6MTY3MjA3NDAwMCwiZXhwIjoxNzAwNTg2MDAwfQ.nqyOmcwBXyqINN0ROA_xI8TKx0Jk05_lwRy4Cdv0j_8`,
+        option:''
+    }
+    if(getStorageJson(USER_LOGIN)){
+        req.headers = {...req.headers,
+            Authorization:`Bearer ${getStorageJson(USER_LOGIN).accessToken}`
+        }
+    }
+    return req
+})
+
+//cấu hình interceptor cho response nhận về
+http.interceptors.response.use((response)=>{
+    console.log(response,'response');
+    return response;
+},error=>{
+    if(error.response?.status){
+        console.log(error.response.status,'status');
+        if(error.response.status === 404){
+            console.log('404 status');
+            history.push('register');
+        }
+    }
+})
